d4243971536c508c8b5f07102d6a7d81c35bd59e
ffpy <q1411603774@163.com>
2021-06-11 17:56:20 +0800

    commit

--- src/main/kotlin/org/ffpy/socketforward/main.kt
+++ src/main/kotlin/org/ffpy/socketforward/main.kt
@@ -1,11 +1,9 @@
 package org.ffpy.socketforward
 
-import cn.hutool.core.util.RandomUtil
 import org.ffpy.socketforward.util.JsonUtils
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.nio.file.Path
-import java.util.concurrent.TimeUnit
 
 val log: Logger = LoggerFactory.getLogger("org.ffpy.socketforward.MainKt")
 
@@ -17,11 +15,7 @@ fun main(vararg args: String) {
         return
     }
     val config = JsonUtils.parse(configPath, Config::class.java)
-
-    while (true) {
-        log.info(RandomUtil.randomString(10))
-        TimeUnit.SECONDS.sleep(1)
-    }
+    log.info(config.toString())
 }
 
 
